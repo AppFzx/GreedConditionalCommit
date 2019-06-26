de57671c43529cf03d56064497a7c725669f1c1f
Tony Slagle <tonyslagle@Tonys-MacBook-Pro.local>
2019-06-26 00:17:44 -0400

    AutoCommit

--- greed.py
+++ greed.py
@@ -1,2 +1,10 @@
 # greed.py - prototype implementation demoing test && commit || revert
 
+class Greed:
+    """ Hold dice and evaluate scores for rolls """
+    def __init__(self, dice = []):
+        self.dice = dice
+
+    @classmethod
+    def getScore(self):
+        return 0
--- test_greed.py
+++ test_greed.py
@@ -1,2 +1,13 @@
-def greed(self, dice):
-    pass
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
+
+# def rollNew(cls, howMany):
+#     import random
+#     ret = cls()
+#     for int i in range(howMany):
+#         ret.dice.append(random.randint(1,6))
+#     return ret
+
+if __name__ == "__main__":
+    test_sum()
+    print("Greedy grin given - committing")
