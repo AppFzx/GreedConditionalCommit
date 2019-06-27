de17679137391efbbeddd07ab2375169fce7e04c
Tony Slagle <tonyslagle@Tonys-MacBook-Pro.local>
2019-06-26 21:35:02 -0400

    Rename and add some untested changes

--- greed_game.py
+++ greed_game.py
@@ -5,6 +5,17 @@ class Greed:
     def __init__(self, dice = []):
         self.dice = dice
 
+    def __str__(self):
+        return ','.join(self.dice)
+
+    def __repr__(self):
+        return ','.join(self.dice)
+
     @classmethod
+    def rollRandom(cls, )
+
     def getScore(self):
         return 0
+
+if __name__ == "__main__":
+    g = Greed()
