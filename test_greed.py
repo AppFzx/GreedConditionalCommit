9118f99b49bddafbee993cd279b9c4c02a883a78
Tony Slagle <tonyslagle@Tonys-MacBook-Pro.local>
2019-06-26 20:48:39 -0400

    Update .gitignore with .vscode

--- test_greed.py
+++ test_greed.py
@@ -2,7 +2,7 @@ def test_sum():
     assert sum([1, 2, 3]) == 6, "Should be 6"
 
 def test_fail():
-    assert 1 == 2, "1 == 2 failed"
+    assert 1 == 1, "1 == 2 failed"
 
 # def rollNew(cls, howMany):
 #     import random
